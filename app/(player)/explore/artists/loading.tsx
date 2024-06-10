a54015c67af1cf8f0b226946043b4bfced6a467b
saeed1202 <sakhshijan@gmail.com>
2024-06-10 17:10:04 +0330

    finish.

--- app/(player)/explore/artists/loading.tsx
+++ app/(player)/explore/artists/loading.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Loading = () => {
-  return <div>loading</div>;
+ return (
+  <div className="flex min-h-screen w-full">
+   <div className="w-12 bg-black" />
+   <div className="flex-1 bg-zinc-950"></div>
+   <div className="w-12 bg-black" />
+  </div>
+ );
 };
 
 export default Loading;
